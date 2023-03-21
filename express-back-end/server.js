a6d07f68011aef867beaa416a28487c5f78f9e6c
Ethan Wong <ethanwong98@yahoo.ca>
2023-03-20 17:14:25 -0700

    setup database

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const Express = require('express');
 const App = Express();
 const BodyParser = require('body-parser');
 const PORT = 8080;
+const connection = require('./db/connection.js');
 
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
@@ -9,13 +11,15 @@ App.use(BodyParser.json());
 App.use(Express.static('public'));
 
 // Sample GET route
-App.get('/api/data', (req, res) => res.json({
-  message: "Seems to work!",
-}));
-
-
+App.get('/api/data', (req, res) => {
+	connection.query('SELECT * FROM users').then((data) => {
+		res.json(data.rows);
+	});
+});
 
 App.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
+	// eslint-disable-next-line no-console
+	console.log(
+		`Express seems to be listening on port ${PORT} so that's pretty good 👍`
+	);
 });
