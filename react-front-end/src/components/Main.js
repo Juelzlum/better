07074da5efc7e5afdd05547a1eb0b0e63b7b5918
Allen Ding <allen.ding@hotmail.com>
2023-03-22 14:48:51 -0700

    add css to main component

--- react-front-end/src/components/Main.js
+++ react-front-end/src/components/Main.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactCalendar from './calendar/Calendar';
-
+import './Main.css'
 
 const Main = () => {
   return (
     <div className="main">
+      <h1 className="title">Better</h1>
       <ReactCalendar />
     </div>
   )
