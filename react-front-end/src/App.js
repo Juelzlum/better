c1dc5234dac1f4aec56e910778ad4f3262b233d7
Allen Ding <allen.ding@hotmail.com>
2023-03-21 17:18:55 -0700

    install calendar dependencies and implemented a basic calendar

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './components/Home';
+import ReactCalendar from './components/calendar/Calendar';
 
 class App extends Component {
 	constructor(props) {
@@ -34,6 +35,7 @@ class App extends Component {
 					<Routes>
 						<Route path='/' element={<Home />} />
 						<Route path='/login' element={<Login />} />
+						<Route path='/calendar' element={<ReactCalendar />} />
 					</Routes>
 				</Router>
 			</div>
