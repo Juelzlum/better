fb0d1c2cb2d8c5f08cfd5bf39e4613f1d011291a
Allen Ding <allen.ding@hotmail.com>
2023-03-22 19:27:56 -0700

    start new branch

--- react-front-end/src/components/calendar/Calendar.js
+++ react-front-end/src/components/calendar/Calendar.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import './Calendar.css'
 
+
 const ReactCalendar = () => {
   const [date, setDate] = useState(new Date());
 
